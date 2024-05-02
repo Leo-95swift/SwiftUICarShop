d4c978d512af98570ec27b32f6227c957a63b47d
xach55 <xach55@gmail.com>
2024-05-02 21:25:28 +0400

    initial commit

---
+++ SwiftUICarShop/SwiftUICarShop/SwiftUICarShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICarShopApp.swift
+//  SwiftUICarShop
+//
+//  Created by Levon Shaxbazyan on 02.05.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICarShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
