511194a79299c7fbbd765d0fe1788eb8053fbd46
xach55 <xach55@gmail.com>
2024-05-03 12:24:31 +0400

    закончил проект CarShop

---
+++ SwiftUICarShop/SwiftUICarShop/Modules/CarShopModule/Model/InsureToggle.swift
@@ -0,0 +1,13 @@
+//
+//  InsureToggle.swift
+//  SwiftUICarShop
+//
+//  Created by Levon Shaxbazyan on 03.05.24.
+//
+
+import Foundation
+
+public enum InsureToggle {
+    case isOn
+    case isOff
+}
